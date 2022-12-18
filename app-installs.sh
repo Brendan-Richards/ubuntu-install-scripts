2fb1bec2dd85be10c15f30cdbceeccd7e449a128
Brendan <quend00@gmail.com>
2022-12-17 21:07:45 -0500

    new files

--- app-installs.sh
+++ app-installs.sh
@@ -105,6 +105,16 @@ sudo add-apt-repository ppa:phoerious/keepassxc -y
 sudo apt update
 sudo apt install keepassxc -y
 
+# install cuda and cudnn
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda-repo-ubuntu2204-12-0-local_12.0.0-525.60.13-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-0-local_12.0.0-525.60.13-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda
+
+
 # install reaper
 cd ~
 wget -P ~ "https://www.reaper.fm/files/6.x/reaper670_linux_x86_64.tar.xz"
