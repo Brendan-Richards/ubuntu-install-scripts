2231988a707a6dd7aee36d199cfd681c8c6e07b9
brendan <quend00@gmail.com>
2025-09-01 17:35:17 -0600

    remove old apps

--- app-installs.sh
+++ app-installs.sh
@@ -55,21 +55,8 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f -y
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-# install pyenv
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
-# allows us to source the bashrc in a non-interactive shell. probably not good practice
-export PS1=false
-source ~/.bashrc
-pyenv install 3.10.0
-pyenv global 3.10.0
-
-# install poetry
-curl -sSL https://install.python-poetry.org | python3 -
-echo 'export PATH="/home/brendan/.local/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+# install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # install vscode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
@@ -105,16 +92,6 @@ sudo add-apt-repository ppa:phoerious/keepassxc -y
 sudo apt update
 sudo apt install keepassxc -y
 
-# install cuda and cudnn
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda-repo-ubuntu2204-12-0-local_12.0.0-525.60.13-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2204-12-0-local_12.0.0-525.60.13-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2204-12-0-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get update
-sudo apt-get -y install cuda
-
-
 # install reaper
 cd ~
 wget -P ~ "https://www.reaper.fm/files/6.x/reaper670_linux_x86_64.tar.xz"
@@ -136,12 +113,6 @@ wine winecfg
 # install winetricks
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y winetricks
-
-# install linvst
-wget https://github.com/osxmidi/LinVst/releases/download/4.78/LinVst-4.78.zip
-unzip LinVst-4.78.zip
-sudo cp LinVst-4.78/linvstconvert /usr/bin
-sudo cp LinVst-4.78/LinVst-4.78-drag-drop/lin* /usr/bin
 
 # install toontrack product manager
 echo "go to this link and download the windows verison: https://www.toontrack.com/product-manager/"
@@ -165,19 +136,13 @@ while [ true ] ; do
         echo "waiting for keypress"
     fi
 done
-echo "Convert toontrack vsts, then exit linvstconvert"
-echo "for top option: navigate to the main linvst file from the download"
-echo "for the bottom option navigate to the vst folder in your ~/.wine/c_drive folders"
-linvstconvert
 
-# install lutris
-sudo apt install -y lutris
-
-# install league of legends
-# wget https://m-reimer.de/wine-lol/debian/wine-lol-glibc_2.33-1_i386.deb
-# wget https://m-reimer.de/wine-lol/debian/wine-lol_5.6-1_i386.deb
-# sudo dpkg -i wine-lol-glibc_2.33-1_i386.deb
-# sudo dpkg -i wine-lol_5.6-1_i386.deb
+# install yahbridge
+wget https://github.com/robbert-vdh/yabridge/releases/download/5.1.1/yabridge-5.1.1.tar.gz
+tar -C ~/.local/share -xavf yabridge-5.1.1.tar.gz
+echo "export PATH='$PATH:$HOME/.local/share/yabridge'" >> ~/.bashrc
+yabridgectl add ~/.wine/drive_c/Program\ Files/VstPlugins
+yabridgectl sync
 
 # install gp7
 wget "https://alt-downloads.guitar-pro.com/gp7/stable/guitar-pro-7-setup.exe"
@@ -185,24 +150,21 @@ wine guitar-pro-7-setup.exe
 
 # install axe fx driver (interactive)
 # add-sudo apt-repository ppa:albaguirre/axe-fx2 && sudo apt -y update && sudo apt install -y axefx2-usb-firmware
-wget http://archive.axefx.fr/AxeFX%20II/Drivers/Axe-Fx_II_USB_Driver_Setup_Linux_106.zip
-unzip Axe-Fx_II_USB_Driver_Setup_Linux_106.zip
-sudo apt install -y fxload
-sudo Axe-Fx_II_USB_Driver_Setup/axefx2setup.sh
+# wget http://archive.axefx.fr/AxeFX%20II/Drivers/Axe-Fx_II_USB_Driver_Setup_Linux_106.zip
+# unzip Axe-Fx_II_USB_Driver_Setup_Linux_106.zip
+# sudo apt install -y fxload
+# sudo Axe-Fx_II_USB_Driver_Setup/axefx2setup.sh
 
 # install axe-edit with wine (interactive) -
-wget "https://www.fractalaudio.com/downloads/axe-edit/Axe-Edit-Win-v3p14p6.exe"
-wine Axe-Edit-Win-v3p14p6.exe
-echo "alias axe-edit='wine \"C:\Program Files (x86)\Fractal Audio\Axe-Edit\Axe-Edit.exe\"'" >> ~/.bashrc
+# wget "https://www.fractalaudio.com/downloads/axe-edit/Axe-Edit-Win-v3p14p6.exe"
+# wine Axe-Edit-Win-v3p14p6.exe
+# echo "alias axe-edit='wine \"C:\Program Files (x86)\Fractal Audio\Axe-Edit\Axe-Edit.exe\"'" >> ~/.bashrc
 
 # install jack
-sudo apt-get install -y qjackctl pulseaudio-module-jack
+sudo apt-get install -y qjackctl
 echo $'\n'
 echo "In QJackctl"
-echo "go to setup -> Misc, check 'Enable Jack D-Bus interface', and uncheck 'Replace Connections with Graph Button'"
-echo "go to setup -> Options, in Execute script after Startup put: 'pacmd load-module module-jack-source channels=2; pacmd load-module module-jack-sink channels=2;'"
 echo "In setup -> Settings -> Advanced, select focusrite 2i2 for input device and output device"
-echo "Set up connections in the main connect menu like the image in the install script repo"
 echo "press any key when done..."
 while [ true ] ; do
     read -t 100 -n 1
@@ -213,21 +175,5 @@ while [ true ] ; do
     fi
 done
 
-# install android studio (interactive)
-wget https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.26/android-studio-2023.1.1.26-linux.tar.gz
-tar -xzvf android-studio-2023.1.1.26-linux.tar.gz
-echo "[Desktop Entry]
-Version=1.0
-Type=Application
-Terminal=true
-Name=Android Studio
-Comment=Launches Android Studio
-Icon=/home/brendan/android-studio/bin/studio.png
-Exec=/home/brendan/android-studio/bin/studio.sh" | sudo tee /usr/share/applications/android-studio.desktop
-
 # set favorites bar
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'snap-store_ubuntu-software.desktop', 'org.gnome.Terminal.desktop', 'google-chrome.desktop', 'org.keepassxc.KeePassXC.desktop', 'spotify.desktop', 'discord.desktop', 'steam.desktop', 'code.desktop', 'cockos-reaper.desktop', 'org.rncbc.qjackctl.desktop', 'wine-Programs-Fractal Audio-Axe-Edit.desktop', 'vlc.desktop', 'wine-Programs-Guitar Pro 7-Guitar Pro 7.desktop', 'net.lutris.Lutris.desktop', 'android-studio.desktop']"
-
-# install dropbox (interactive)
-cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-/home/brendan/.dropbox-dist/dropboxd
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Nautilus.desktop', 'snap-store_ubuntu-software.desktop', 'org.gnome.Terminal.desktop', 'google-chrome.desktop', 'org.keepassxc.KeePassXC.desktop', 'spotify.desktop', 'discord.desktop', 'steam.desktop', 'code.desktop', 'cockos-reaper.desktop', 'org.rncbc.qjackctl.desktop', 'wine-Programs-Guitar Pro 7-Guitar Pro 7.desktop']"
